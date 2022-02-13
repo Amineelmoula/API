39538f004b9c35d574e2f299930190dd7b2140b5
unknown <eliyji666@gmail.com>
2022-02-13 18:35:16 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import UserList from "./UserList";
+
+function App() {
+  return (
+    <div className="app">
+      <h1 className="title">DATA</h1>
+      <UserList />
+    </div>
+  );
+}
+
+export default App;
